b581ff3b2da9649481a51c00145ff3b2c1dc5107
gustavo <gustavo.pereira09@escola.pr.gov.br>
2026-08-17 13:21:34 +0000

    Adiciona arquivos de exemplo para o módulo ESM e implementa servidor básico em Node.js

---
+++ 2°TRIMESTRE/modulos/mod03-server/server1.js
@@ -0,0 +1,34 @@
+const http = require('node:http')
+const path = require('node:path')
+const fs = require('node:fs')
+const porta = 8005
+
+const home = `path.join(__dirname, 'pages/index.html')`
+const sobre = `path.join(__dirname, 'pages/sobre.html')`
+
+const server = http.createServer((req, res) =>{
+    const urlTratada = new URL(req.url, `http://${req.headers.host}`)
+    const recursos = urlTratada.pathname
+
+    if(recursos === '/sobre'){
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        return res.end(fs.readFileSync(home, 'utf-8'))
+
+    } if (recursos === '/'){
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        return res.end(fs.readFileSync(sobre, 'utf-8'))
+    } else {
+        res.statusCode = 401
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.end('401 Não autorizado')
+    }
+
+})
+
+server.listen(porta, () => {
+    console.log(`Servidor rodando na porta ${porta}`)
+
+
+})
