7382ebdc502fc7cd522c7b531608da19b7cef246
gustavo <gustavo.pereira09@escola.pr.gov.br>
2026-09-02 12:31:14 +0000

    feat: initialize express application with basic server setup
    - Added package.json for express application with express as a dependency.
    - Created servidor.js to set up a basic Express server with two routes.
    - Removed previous package-lock.json and package.json related to chalk dependency.

---
+++ 3ºTRIMESTRE/express01/servidor.js
@@ -0,0 +1,14 @@
+// importação moderna com ESM (import)
+import express from 'express'
+
+const app = express() // Instanciar Express
+
+app.get('/',(req, res) => {
+    res.send(`<h3>Pagina Inicial</h3>\n
+        <p>Loja especializada em roupas</p>`)
+})
+app.get('/',(req, res) => {
+    res.send('Minha roupa nova....!')
+})
+
+app.listen(3000, ()=> {console.log('Servidor está vivo')})
